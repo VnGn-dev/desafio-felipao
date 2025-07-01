898256f9f21d5dabb7c7cf718680cb093aa0387e
VnGn-dev <vinicius.goncalves.dev@gmail.com>
2025-06-30 23:49:43 -0300

    Desafio Finalizado!

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
-let nome = "Felipe";
-console.log (nome);
+let nomeDoHeroi = "Felipe"
+let QuantidadeDeXp = 30000
+let nivelDoHeroi = ''
+
+    if (QuantidadeDeXp <=1000) {
+        nivelDoHeroi = "Ferro"   
+    }else if (QuantidadeDeXp <= 2000) {
+        nivelDoHeroi = "Bronze"
+    }else if (QuantidadeDeXp <= 5000) {
+        nivelDoHeroi = "Prata"
+    }else if (QuantidadeDeXp <= 7000) {
+        nivelDoHeroi = "Ouro"
+    }else if (QuantidadeDeXp <= 8000) {
+        nivelDoHeroi = "Platina"
+    }else if (QuantidadeDeXp <= 9000) {
+        nivelDoHeroi = "Ascendente"
+    }else if (QuantidadeDeXp <= 10000) {
+        nivelDoHeroi = "Imortal"
+    }else {
+        nivelDoHeroi = "Radiante"
+    }
+    console.log("O Heroi de nome " + nomeDoHeroi + " esta no nivel " + nivelDoHeroi)  
